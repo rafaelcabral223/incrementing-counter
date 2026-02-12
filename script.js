5ea5b4099eb958da14a4a379b24476286f7be3a5
Rafael Cabral - Majora Capital <rafael.cabral@majora.capital>
2026-02-12 12:37:37 -0300

    código final

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const counters = document.querySelectorAll('.counter')
 
+counters.forEach(counter => {
+    counter.innerText = '0'
+
+    const upadateCounter = () => {
+        const target = +counter.getAttribute('data-target')
+        const c = +counter.innerText
+
+        const increment = target / 500
+
+        if (c <target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(upadateCounter, 1)
+        } else {
+            counter.innerText = target
+        }
+    }
+
+    upadateCounter()
+})
